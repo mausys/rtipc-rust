c03e38d0fee91beda473a589a86ab8c0d4ca2e52
Simon Maurer <mail@maurer.systems>
2025-09-25 13:20:52 +0200

    update header for slice

--- src/header.rs
+++ src/header.rs
@@ -1,6 +1,7 @@
+use std::mem::size_of;
+
 use crate::cache::max_cacheline_size;
 use crate::error::*;
-use crate::shm::Chunk;
 use crate::Index;
 
 const RTIC_MAGIC: u16 = 0x1f0c;
@@ -14,56 +15,51 @@ pub struct Header {
     /**< cookie for object protocol */
     cacheline_size: u16,
     atomic_size: u16,
-    /**< number of channels for producers / consumers */
-    pub num_channels: [u32; 2],
 }
 
-impl Header {
-    pub(crate) fn new(num_consumers: u32, num_producers: u32) -> Self {
-        let cacheline_size: u16 = max_cacheline_size().try_into().unwrap();
-        let atomic_size: u16 = std::mem::size_of::<Index>().try_into().unwrap();
-        Header {
-            magic: RTIC_MAGIC,
-            version: RTIC_VERSION,
-            num_channels: [num_consumers, num_producers],
-            cacheline_size,
-            atomic_size,
-        }
+
+pub(crate) fn check_header(buf: &[u8; size_of::<Header>()]) -> Result<(), HeaderError> {
+    let cacheline_size: u16 = max_cacheline_size().try_into().unwrap();
+    let atomic_size: u16 = std::mem::size_of::<Index>().try_into().unwrap();
+    let ptr: *const Header = buf.as_ptr() as  *const Header;
+
+    let header = unsafe { ptr.read() };
+
+    if header.magic != RTIC_MAGIC {
+        return Err(HeaderError::Magic);
     }
 
-    pub(crate) fn from_chunk(chunk: &Chunk) -> Result<Header, HeaderError> {
-        let cacheline_size: u16 = max_cacheline_size().try_into().unwrap();
-        let atomic_size: u16 = std::mem::size_of::<Index>().try_into().unwrap();
-        let ptr: *const Header = chunk.get_ptr(0)?;
-
-        let header = unsafe { ptr.read() };
-
-        if header.magic != RTIC_MAGIC {
-            return Err(HeaderError::Magic);
-        }
-
-        if header.version != RTIC_VERSION {
-            return Err(HeaderError::Version);
-        }
-
-        if header.cacheline_size != cacheline_size {
-            return Err(HeaderError::CachelineSize);
-        }
-
-        if header.atomic_size != atomic_size {
-            return Err(HeaderError::AtomicSize);
-        }
-
-        Ok(header)
+    if header.version != RTIC_VERSION {
+        return Err(HeaderError::Version);
     }
 
-    pub(crate) fn write(&self, chunk: &Chunk) -> Result<(), MemError> {
-        let ptr: *mut Header = chunk.get_ptr(0)?;
-
-        unsafe {
-            std::ptr::write(ptr, *self);
-        }
-
-        Ok(())
+    if header.cacheline_size != cacheline_size {
+        return Err(HeaderError::CachelineSize);
     }
+
+    if header.atomic_size != atomic_size {
+        return Err(HeaderError::AtomicSize);
+    }
+
+    Ok(())
+}
+
+pub(crate) fn write_header(buf: &mut [u8; size_of::<Header>()]) {
+
+    let cacheline_size: u16 = max_cacheline_size().try_into().unwrap();
+    let atomic_size: u16 = std::mem::size_of::<Index>().try_into().unwrap();
+
+    let header =  Header {
+        magic: RTIC_MAGIC,
+        version: RTIC_VERSION,
+        cacheline_size,
+        atomic_size,
+    };
+
+    let ptr: *mut Header = buf.as_ptr() as  *mut Header;
+
+
+    unsafe {
+        std::ptr::write(ptr, header);
+    };
 }
